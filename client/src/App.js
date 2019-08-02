43c5a187e5f5141057858f4c2aa1782bf219a26f
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-08-02 02:08:16 +0100

    made the card wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import EventList from './components/EventList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <EventList />
     </div>
   );
 }
---
+++ client/src/components/EventList.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          
+          title="Google DevFest 2019"
+          subheader="August 14, 2019"
+        />
+        <CardMedia
+          className={classes.media}
+          style = {{ height: 0, paddingTop: '56%'}}
+          image={require('../images/cards/devfest.jpg')}
+          title="Google devfest"
+        />
+        <CardContent>
+          <Typography component="p">
+          Join us for the annual developer festival of sessions, workshops, training, codelabs 
+          and much more by awesome speakers and with nearly 2,000 other developers from across 
+          the South West Region of Nigeria.
+          </Typography>
+        </CardContent>
+        
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
