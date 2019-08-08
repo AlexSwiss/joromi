9bf29f99232e6e64b8763c994100e125cb2ddb98
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-08-08 22:21:16 +0100

    Event list reading from state

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -8,6 +8,59 @@ import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
+import uuid from 'uuid';
+
+
+class RecipeReviewCard extends Component {
+
+  state = {
+    events: [
+                {id: uuid(), name: 'Youth Fest', venue: 'Gonin gora', price: '3000'},
+                {id: uuid(), name: 'Musical Concert', venue: 'Space 3000', price: '50000'}
+            ]
+    }
+
+  render() {
+    const { classes } = this.props;
+    const { events } = this.state
+
+    return (
+      <div>
+      
+       { events.map(({ id, name}) => (
+      
+      <Card className={classes.card}>
+
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          
+          title={name}
+          subheader="August 14, 2019"
+          key={id}
+          timeout={500}
+        />
+        <CardMedia
+          className={classes.media}
+          style = {{ height: 0, paddingTop: '56%'}}
+          image={require('../images/cards/devfest.jpg')}
+          title="Google devfest"
+        ></CardMedia>
+        <CardContent>
+          <Typography component="p">
+          Join us for the annual developer festival of sessions.
+          </Typography>
+        </CardContent>
+
+      </Card>
+      ))}
+      </div>
+    );
+  }
+}
 
 const styles = theme => ({
   card: {
@@ -35,41 +88,6 @@ const styles = theme => ({
   },
 });
 
-class RecipeReviewCard extends React.Component {
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" className={classes.avatar}>
-              R
-            </Avatar>
-          }
-          
-          title="Google DevFest 2019"
-          subheader="August 14, 2019"
-        />
-        <CardMedia
-          className={classes.media}
-          style = {{ height: 0, paddingTop: '56%'}}
-          image={require('../images/cards/devfest.jpg')}
-          title="Google devfest"
-        />
-        <CardContent>
-          <Typography component="p">
-          Join us for the annual developer festival of sessions, workshops, training, codelabs 
-          and much more by awesome speakers and with nearly 2,000 other developers from across 
-          the South West Region of Nigeria.
-          </Typography>
-        </CardContent>
-        
-      </Card>
-    );
-  }
-}
 
 RecipeReviewCard.propTypes = {
   classes: PropTypes.object.isRequired,
